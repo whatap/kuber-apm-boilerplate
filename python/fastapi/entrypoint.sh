d096525dd72b7a34916932aec1fa890484f4c7b8
jykim <jykim@whatap.io>
2023-09-03 00:42:54 +0900

    updated fastapi-app-name

--- python/fastapi/entrypoint.sh
+++ python/fastapi/entrypoint.sh
@@ -4,7 +4,7 @@ chmod -R 777 $WHATAP_HOME
 whatap-setting-config \
 --host 15.165.146.117 \
 --license $WHATAP_LICENSE \
---app_name myapp \
+--app_name JOB-TRIGGER \
 --app_process_name uvicorn
 
 whatap-start-agent uvicorn server:app --host 0.0.0.0 --port 8000
