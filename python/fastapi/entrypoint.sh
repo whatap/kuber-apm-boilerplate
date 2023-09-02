69310e856d121e8203b32264f59a59856915c2c0
jykim <jykim@whatap.io>
2023-09-03 02:00:23 +0900

    updated fastapi(yaml)

--- python/fastapi/entrypoint.sh
+++ python/fastapi/entrypoint.sh
@@ -2,9 +2,12 @@
 export WHATAP_HOME=${PWD}
 chmod -R 777 $WHATAP_HOME
 whatap-setting-config \
---host 15.165.146.117 \
+--host $WHATAP_LICENSE \
 --license $WHATAP_LICENSE \
 --app_name $WHATAP_APP_NAME \
 --app_process_name uvicorn
 
+echo "trace_logging_enabled=${WHATAP_LOGGING_ENABLED}" >> whatap.conf
+echo "log_unhandled_exception=${WHATAP_LOGGING_ENABLED}" >> whatap.conf
+echo "profile_basetime=0" >> whatap.conf
 whatap-start-agent uvicorn server:app --host 0.0.0.0 --port 8000
