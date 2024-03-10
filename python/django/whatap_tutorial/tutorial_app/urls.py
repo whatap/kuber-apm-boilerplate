475b8349756b97d59be0e03b1bcaeff5a984b83b
jykim <jykim@whatap.io>
2024-03-10 17:03:11 +0900

    active stack check

--- python/django/whatap_tutorial/tutorial_app/urls.py
+++ python/django/whatap_tutorial/tutorial_app/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path("", views.home, name="home"),
     path("health_check/", views.health_check, name="health_check"),
+    path("active_stack_check/", views.active_stack_check, name="active_stack_check"),
     path("error_check/", views.error_check, name="error_check")
 ]
-
