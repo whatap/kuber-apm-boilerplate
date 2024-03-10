0ee1933af9198f333bace6b13ce41880078256d3
jykim <jykim@whatap.io>
2024-03-10 17:54:42 +0900

    fixed urls.py

--- python/django/whatap_tutorial/tutorial_app/urls.py
+++ python/django/whatap_tutorial/tutorial_app/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
-    path("health_check/", views.health_check, name="health_check"),
-    path("active_stack_check/", views.active_stack_check, name="active_stack_check"),
-    path("error_check/", views.error_check, name="error_check")
+    path("health_check", views.health_check, name="health_check"),
+    path("active_stack_check", views.active_stack_check, name="active_stack_check"),
+    path("error_check", views.error_check, name="error_check")
 ]
